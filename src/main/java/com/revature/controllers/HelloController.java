eb65a8cc5e48a2829e20f388c8dba45a66514523
Mitch Goshorn <msgoshorn@gmail.com>
2019-05-16 15:47:29 -0400

    Added post functionality

--- src/main/java/com/revature/controllers/HelloController.java
+++ src/main/java/com/revature/controllers/HelloController.java
@@ -1,6 +1,8 @@
 package com.revature.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,5 +13,10 @@ public class HelloController {
 	@GetMapping("")
 	public String sayHello() {
 		return "Hello";
+	}
+	
+	@PostMapping("")
+	public String sayHello(@RequestBody String name) {
+		return "Hello " + name + "!";
 	}
 }
